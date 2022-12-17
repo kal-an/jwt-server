a4d547de979f8dedab7089ff45d0267356018d1e
Andrey <dev.andr@yandex.com>
2022-12-17 23:28:15 +0500

    feat: added class jwt for response

---
+++ src/main/java/com/organization/jwtserver/domain/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.organization.jwtserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JwtResponse {
+
+    private final String type = "Bearer";
+    private String accessToken;
+    private String refreshToken;
+}
