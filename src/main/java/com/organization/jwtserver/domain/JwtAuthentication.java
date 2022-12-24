768bd304937e847030de744a06b5d8a94be4a8cc
Andrey <dev.andr@yandex.com>
2022-12-24 21:37:09 +0500

    feat: added class JwtAuthentication

---
+++ src/main/java/com/organization/jwtserver/domain/JwtAuthentication.java
@@ -0,0 +1,54 @@
+package com.organization.jwtserver.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Getter
+@Setter
+public class JwtAuthentication implements Authentication {
+
+    private boolean authenticated;
+    private String username;
+    private String firstName;
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return false;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
