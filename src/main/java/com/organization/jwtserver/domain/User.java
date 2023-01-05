c6903cc13287b54e765a5a338714c8530dc70ff8
Andrey <dev.andr@yandex.com>
2023-01-05 22:12:47 +0500

    fix: imports in User

--- src/main/java/com/organization/jwtserver/domain/User.java
+++ src/main/java/com/organization/jwtserver/domain/User.java
@@ -1,11 +1,11 @@
 package com.organization.jwtserver.domain;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.Set;
 
 @AllArgsConstructor
