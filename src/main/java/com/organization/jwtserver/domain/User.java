d7ac437e3e30d98ff4669196b9680d7882d7daf6
Andrey <dev.andr@yandex.com>
2023-01-05 21:33:45 +0500

    feat: added entity annotations to user

--- src/main/java/com/organization/jwtserver/domain/User.java
+++ src/main/java/com/organization/jwtserver/domain/User.java
@@ -1,5 +1,6 @@
 package com.organization.jwtserver.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +12,26 @@ import java.util.Set;
 @Setter
 @Getter
 @NoArgsConstructor
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
     private String login;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String firstname;
+
+    @Column(nullable = false)
     private String lastname;
+
+    @ElementCollection
+    @Column(nullable = false)
     private Set<Role> roles;
 }
