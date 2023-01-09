d41d18071e44c906aa309625f440709950bb1698
Andrey <dev.andr@yandex.com>
2023-01-09 20:33:04 +0500

    feat: created data.sql, added init.mode to properties

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -15,8 +15,3 @@ create table if not exists roles (
     CONSTRAINT fk_role_on_user FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-insert into users (login, password, first_name, last_name) values ('andrey', '1234', 'andrey', 'k');
-insert into users (login, password, first_name, last_name) values ('anton', '1234', 'anton', 'k');
-insert into roles (user_id, role) values (1, 'admin');
-insert into roles (user_id, role) values (2, 'user');
-
