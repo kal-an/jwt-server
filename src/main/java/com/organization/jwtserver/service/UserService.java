0c209b37872c4962898457e1f7abce1161b9d4ec
Andrey <dev.andr@yandex.com>
2023-01-10 16:17:47 +0500

    style: added comments to interface UserService

--- src/main/java/com/organization/jwtserver/service/UserService.java
+++ src/main/java/com/organization/jwtserver/service/UserService.java
@@ -1,11 +1,18 @@
 package com.organization.jwtserver.service;
 
 import com.organization.jwtserver.domain.User;
+import com.organization.jwtserver.exception.AuthException;
 
 import java.util.Optional;
 
 public interface UserService {
 
+    /**
+     * Получение пользователя по логину
+     * @param login строка с логином
+     * @return {@link User} обернутый в {@link Optional}
+     * @throws AuthException если пользователь не найден или пароль неверный
+     */
     Optional<User> getUserByLogin(String login);
 
 }
